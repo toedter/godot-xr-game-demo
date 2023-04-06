f27785271c444edc8fb0065e52ad8a672387beb9
Kai Toedter <kai@toedter.com>
2023-04-06 07:42:31 +0200

    feat: add teleport

--- levels/level-2/level-2.gd
+++ levels/level-2/level-2.gd
@@ -4,13 +4,28 @@ extends DemoSceneBase
 @onready var origin_node : XROrigin3D = XRHelpers.get_xr_origin(self)
 @onready var start_origin := origin_node.global_transform
 
+@onready var collected_coins := 0;
+@onready var coins_in_level := 0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
+	for star in get_node("stars").get_children():
+		star.connect("coin_collected", _on_coin_collected)
+		coins_in_level += 1
 
 
 func _on_fall_damage_player_fall_damage(_damage):
 	origin_node.global_transform = start_origin
 	if !$AudioStreamPlayer.playing:
 		$AudioStreamPlayer.play()
+
+func _on_coin_collected():
+	collected_coins += 1
+	
+	if !$StarAudioStreamPlayer3D.playing:
+		$StarAudioStreamPlayer3D.play()
+	
+	if collected_coins == coins_in_level:
+		print("GAME WON!")
 
