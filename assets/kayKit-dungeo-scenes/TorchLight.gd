86b16b910e3be20eb8f93003669ac8626f35fbef
Kai Toedter <kai@toedter.com>
2023-04-18 20:01:01 +0200

    chore: polishing torchlight

---
+++ assets/kayKit-dungeo-scenes/TorchLight.gd
@@ -0,0 +1,28 @@
+extends OmniLight3D
+
+@onready var noise = FastNoiseLite.new()
+@onready var position_x_ori = position.x
+@onready var position_y_ori = position.y
+@onready var position_z_ori = position.z
+
+var value = 0.0
+var direction = 1
+
+func _ready():
+	randomize()
+	value = randf()
+	print(value)
+	
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	light_energy = value
+		
+	value += (randf() / 30 * direction);
+	if value > 1.0:
+		direction = -1;
+	if value < 0.9:
+		direction = 1;
+	
+	
