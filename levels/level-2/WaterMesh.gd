ec1507123d0abdc592a82a0a3dc210c381448cdd
Kai Toedter <kai@toedter.com>
2024-08-19 12:49:24 +0200

    fix: fixed water for Godot 4.3

--- levels/level-2/WaterMesh.gd
+++ levels/level-2/WaterMesh.gd
@@ -14,8 +14,8 @@ func _ready():
 	material = mesh.surface_get_material(0)
 	noise = material.get_shader_parameter("wave").noise.get_seamless_image(512, 512)
 	noise_scale = material.get_shader_parameter("noise_scale")
-	wave_speed = material.get_shader_parameter("wave_speed")
-	height_scale = material.get_shader_parameter("height_scale")
+#	wave_speed = material.get_shader_parameter("wave_speed")
+#	height_scale = material.get_shader_parameter("height_scale")
 
 
 
